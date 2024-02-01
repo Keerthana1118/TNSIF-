8188da86b4cf3a219f1c5063f7f1105de21cc5b7
My PC <My PC@10.41.136.84>
2024-02-01 13:23:20 +0530

    Second Commit

--- src/com/tnsif/day10/interimpl/BankApplication.java
+++ src/com/tnsif/day10/interimpl/BankApplication.java
@@ -1,5 +1,22 @@
 package com.tnsif.day10.interimpl;
 
 public class BankApplication {
+	 public static void main(String[] args) {
+	     // Create a Bank instance
+	     Bank bank = new BankImpl();
+
+	     // Create an Account
+	     Account account = new Account(123456, "Mihir", 155000, bank);
+
+	     // Deposit money
+	     account.deposit(20000);
+	     // Print the account details
+	     System.out.println(account);
+	     
+	     //withdraw money
+	     account.withdraw(10);
+	     // Print the account details
+	     System.out.println(account);
+	 }
 
 }
---
+++ src/com/tnsif/day10/markerint/Registrable.java
@@ -0,0 +1,5 @@
+package com.tnsif.day10.markerint;
+
+public interface Registrable {
+
+}
